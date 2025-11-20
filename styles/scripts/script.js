d41aa2fcdd1310ef7900a78709b38e4544751ee8
Blyoba23 <egorlutyj9@gmail.com>
2025-11-20 14:08:32 +0200

    Implement simple canvas game with keyboard controls
    Added a simple canvas game that allows movement using arrow keys.

--- styles/scripts/script.js
+++ styles/scripts/script.js
@@ -118,4 +118,28 @@ async function loadDogs() {
     }
 }
 
+
 loadDogs();
+
+// Task 6 — simple canvas game 
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+let x = 50, y = 50;
+
+function draw() {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle - "cyan";
+    ctx.fillRect(x, y, 40, 40);
+    
+}
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp") y-= 10;
+    if (e.key === "ArrowDown") y += 10;
+    if (e.key === "ArrowLeft") x -= 10;
+    if (e.key === "ArrowRight") x +=10;
+    draw();
+
+});
+
+draw();
